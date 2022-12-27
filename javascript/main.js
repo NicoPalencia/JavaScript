9d05caa06b2d875b2a1fd61fe4fd6ba304ae057d
NicoPalencia <nicolasfpalencia@gmail.com>
2022-12-27 20:18:22 -0300

    Cambio de proyecto a carrito de compras

--- javascript/main.js
+++ javascript/main.js
@@ -1,54 +1,63 @@
-let PINcorrecto = "1717";
+console.log("inicio")
+console.log(compraDeBaldosas());
 
-function login() {
-    let ingresar = false;
+while (opcion != 0){
+    opcion = baldosas(prompt("Ingrese Producto " + "Baldosas"))
 
-    for (let i = 2; i >= 0; i--) {
-        let PINusuario = prompt("Ingrese su PIN");
-        if (PINusuario == PINcorrecto) {
-            alert("Bienvenido/a. Ingreso Exitoso!");
-            ingresar = true;
+    switch (opcion){
+        case "Baldosas Lisas":
+            cantidad = parseInt(prompt("Cuantas Baldosas Lisas desea comprar?"))
+            if (resultado <= cantidad){
+            total += (cantidad = BaldosasLisas.precio);
+            alert("El valor de la Baldosa Lisa es de $" + total)
+            }
             break;
-        }else {
-            alert("PIN incorrecto. Le quedan " + i + " intentos");
-        }
-    }
-    return ingresar;
-}
 
-let exito = login();
-if (exito) {
-    let saldo = 100000;
+        case "Baldosas Tramadas":
+            cantidad = parseInt(prompt("Cuantas Baldosas Tramadas desea comprar?"))
+            if (resultado <= cantidad){
+            total += (cantidad = BaldosasTramadas.precio);
+            alert ("El valor de la Baldosa Tramada es de $" + total)
+            }
+            break;
 
-    let opcion = prompt("Elija la opcion deseada: \n1 - Saldo. \n2 - Retiro de dinero. \n3 - Depósito de dinero. \nPresione X para finalizar.");
-
-    while (opcion != "X") {
-        switch (opcion) {
-            case "1":
-            alert("Su saldo es $" + saldo);                
-                break;
-            case "2":
-                let retiro = parseInt(prompt("Ingrese el monto a retirar"));
-                if (retiro <= saldo) {
-                    saldo = saldo - retiro;
-                    alert("Retiraste $" + retiro + ", su nuevo saldo es $" + saldo);
-                } else {
-                    alert("Su saldo es insuficiente");
-                }
-                break;
-            case "3":
-                let deposito = parseInt(prompt("Ingrese el monto a depositar"));
-                saldo = saldo + deposito;
-                alert("Su nuevo saldo es $" + saldo);
-                break;
+        case "Baldosas Rombos":
+            cantidad = parseInt(prompt("Cuantas Baldosas Rombos desea comprar?"))
+            if (resultado <= cantidad){
+            total += (cantidad = BaldosasRombos.precio);
+            alert ("El valor de la Baldosas Rombos es de $" + total)
+            }
+            break;
 
             default:
-                alert("Opcion seleccionada no válida");
+                (prompt("Opcion no valida presione boton Salir"))
                 break;
-        }
-        opcion = prompt("Elija la opcion deseada: \n1 - Saldo. \n2 - Retiro de dinero. \n3 - Depósito de dinero. \nPresione X para finalizar.");           
-        }
-    }else{
-        alert("Retuvimos su tarjeta. Por favor acerquese a una sucursal para solucionar el problema.");
     }
-    alert("Gracias por utilizar nuestros servicios!")
+
+        console.log("Gracias por su visita!")
+}
+
+// Array
+function compraDeBaldosas(array , opcion){
+array.push(opcion)
+}
+
+// Array Suma
+function sumar(){
+    let Totalcompra = total;
+    alert("El total de la compra es de: $" + Totalcompra)
+}
+
+// Objetos
+function Producto(nombre, modelo, precio, stock){
+    this.nombre = nombre;
+    this.modelo = incolora;
+    this.precio = parseFloat (precio);
+    this.stock = parseInt (stock);
+}
+
+const BaldosasLisas = new Producto ("Baldosas Lisas", "Incolora", 5000, 100);
+const BaldosasTramadas = new Producto ("Baldosas Tramadas", "Incolora", 6000, 80);
+const BaldosasRombos = new Producto ("Baldosas Rombos", "Incolora", 6200, 75);
+
+console.log("Gracias por su visita")
